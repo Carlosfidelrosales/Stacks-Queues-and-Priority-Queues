7de7d5e4dbbaff57c5bc29190f7f8cee1b48d678
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-12 16:23:19 +0800

    Added a print function to see if the program is working and if the heappush method has been applied.

--- PrioQueuesHeap.py
+++ PrioQueuesHeap.py
@@ -15,22 +15,3 @@ print("These are the following list of countries from the program for applying h
 print(countries)
 print("----------------------------------------------------------------------------------------")
 
-#heappop - always get the first one, while the remaining elements might shuffle a little bit
-from heapq import heappop
-
-print("----------------------------------------------------------------------------------------")
-print("These are the following list of countries from the program for applying heappop method.")
-print(heappop(countries))
-print(heappop(countries))
-print(countries)
-print("----------------------------------------------------------------------------------------")
-
-# Python's Tuple Comparison (First Name, Last Name and Age)
-person1 = ("John", "Brown", 42)
-person2 = ("John", "Doe", 42)
-person3 = ("John", "Doe", 24)
-
-print("----------------------------------------------------------------------------------------")
-print(f"Person 1 < Person 2 is " person1 < person2)
-print(person2 < person3)
-print("----------------------------------------------------------------------------------------")
