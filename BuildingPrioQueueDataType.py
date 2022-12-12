dc85a48ed38249fc9203829c2b0ea797c6b6fb1d
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-12 16:54:25 +0800

    Added 3 priority levels and included a print function to see if the program is working.

--- BuildingPrioQueueDataType.py
+++ BuildingPrioQueueDataType.py
@@ -1,6 +1,8 @@
 from collections import deque
 from heapq import heappop, heappush
 
+# Enqueue_with_priority() method takes two arguments, a priority and a corresponding value, which it then wraps in a tuple and pushes onto the heap using the heapq module. 
+
 class PriorityQueue:
     def __init__(catalog):
         catalog._elements = []
@@ -10,6 +12,18 @@ class PriorityQueue:
 
     def dequeue(catalog):
         return heappop(catalog._elements)
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+alert = PriorityQueue()
+alert.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+alert.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+alert.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+alert.enqueue_with_priority(CRITICAL, "Hazard lights turned on")
+
+print(alert.dequeue())
 
 
 
