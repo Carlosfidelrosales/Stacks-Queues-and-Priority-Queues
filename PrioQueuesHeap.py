de3ba63f34b1798060fcacdce4f16998f6e00120
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-12 16:36:41 +0800

    Python Tuple comparison has been added that includes a print function.

--- PrioQueuesHeap.py
+++ PrioQueuesHeap.py
@@ -25,5 +25,14 @@ print(heappop(countries))
 print(countries)
 print("----------------------------------------------------------------------------------------")
 
+# Python's Tuple Comparison (First Name, Last Name and Age)
+#  Python determines that person1 should go before person2 based on their last names since they share the same first name, but Python doesn’t look at their ages because the ordering is already known. The age becomes important in the second comparison between person2 and person3, who happen to have the same first and last names.
+person1 = ("Juan", "Bata", 60)
+person2 = ("Juan", "Dela Cruz", 60)
+person3 = ("Juan", "Dela Cruz", 35)
 
+print("----------------------------------------------------------------------------------------")
+print(person1 < person2)
+print(person2 < person3)
+print("----------------------------------------------------------------------------------------")
 
