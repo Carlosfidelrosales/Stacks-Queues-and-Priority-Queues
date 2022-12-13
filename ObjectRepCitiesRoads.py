5432eb82d037636dab2dd5acda180995360f6d5b
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 16:22:28 +0800

    Tried something that can tell the details of a city by the help of networkx tool and pygraphviz.

--- ObjectRepCitiesRoads.py
+++ ObjectRepCitiesRoads.py
@@ -1,4 +1,7 @@
+# import networkx as nx
+# print(nx.nx_agraph.read_dot("roadmap.dot"))
+
+#oxford string maps
 import networkx as nx
-print(nx.nx_agraph.read_dot("roadmap.dot"))
-
-
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+print(graph.nodes["oxford"])
