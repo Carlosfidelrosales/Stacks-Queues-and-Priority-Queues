bd83e634f7debffc5efcb073a89f2a620f0243af
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:02:36 +0800

    Used the provided node as a parameter when using the.neighbors() method on a graph.

--- GraphTester.py
+++ GraphTester.py
@@ -2,5 +2,11 @@ from Graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
-print("Prints the nodes of the selected " + str(nodes["oxford"]))
-print(graph)
+# print("Prints the nodes of the selected " + str(nodes["oxford"]))
+# print(graph)
+
+#list of places/cities near oxford
+print("Places that can be found near Oxford are: \n")
+for neighbor in graph.neighbors(nodes["gloucester"]):
+    print(neighbor.name)
+
