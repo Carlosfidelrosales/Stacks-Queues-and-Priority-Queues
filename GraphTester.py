1d5be0f9ce7ced221126c5730bd198888bc6d7ec
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:15:22 +0800

    Added a def function and a for loop to sort the neighbors from nearest to farthest by miles.

--- GraphTester.py
+++ GraphTester.py
@@ -12,6 +12,16 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 #     print(neighbor.name)
 
 #list of places/cities near oxford and its distance
-for neighbor, interval in graph[nodes["oxford"]].items():
-    print(interval["distance"], neighbor.name)
+# for neighbor, interval in graph[nodes["oxford"]].items():
+#     print(interval["distance"], neighbor.name)
 
+
+#sorted the nearest and farthest place near oxford using the sort_by and by_distance def functions.
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(interval):
+    return float(interval["distance"])
+
+for neighbor, interval in sort_by(graph[nodes["oxford"]], by_distance):
+    print(f"{interval['distance']:>3} miles, {neighbor.name}")
