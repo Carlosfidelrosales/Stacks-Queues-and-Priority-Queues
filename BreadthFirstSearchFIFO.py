4d5e0101926750d76779def82b301370e6a820f1
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:34:25 +0800

    Imported the needed modules like networkx and imported the Graph file and added a def function that tell if a city has been granted to it in 20th century.

---
+++ BreadthFirstSearchFIFO.py
@@ -0,0 +1,7 @@
+import networkx as nx
+from Graph import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
