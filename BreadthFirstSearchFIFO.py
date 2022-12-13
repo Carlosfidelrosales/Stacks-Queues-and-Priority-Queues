88ef94cc579bbc89a03f12031a498144e3cc2eb7
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 19:56:40 +0800

    Fixed some error in the program.

--- BreadthFirstSearchFIFO.py
+++ BreadthFirstSearchFIFO.py
@@ -7,26 +7,26 @@ def is_twentieth_century(year):
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 #for loop that will scan if a city has been granted to a place from 20th century.
-# print("\nFinding the declared city stating from 20th Century.\n\n")
-# for node in nx.bfs_tree(graph, nodes["oxford"]):
-#     print(">>", node.name, node.year)
-#     if is_twentieth_century(node.year):
-#         print("\n\nFound:", node.name, node.year)
-#         break
-# else:
-#     print("Not found")
+print("\n>> Finding the declared city stating from 20th Century.\n\n")
+for node in nx.bfs_tree(graph, nodes["oxford"]):
+    print(">>", node.name)
+    if is_twentieth_century(node.year):
+        print("\n\nFound:", node.name, node.year)
+        break
+    else:
+        print("Not found")
 
 #for loop that will scan if a city is from 20th century and it has the highest latitude among all the others.
-print("\nFinding the City that is from 20th Century and it has highest value of latitude.\n\n")
+print("\n// Finding the City that is from 20th Century and it has highest value of latitude.\n\n")
 def order(neighbors):
     def by_latitude(city):
         return city.latitude
     return iter(sorted(neighbors, key=by_latitude, reverse=True))
 
-for node in nx.bfs_tree(graph, nodes["oxford"], sort_neighbors=order):
-    print(">>", node.name, node.year, node.latitude)
+for node in nx.bfs_tree(graph, nodes["oxford"], order):
+    print("//", node.name)
     if is_twentieth_century(node.year):
         print("\n\nFound:", node.name, node.year, node.latitude)
         break
-else:
-    print("Not found")
+    else:
+        print("Not found")
