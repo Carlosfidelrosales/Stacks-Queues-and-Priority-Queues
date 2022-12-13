bb04eb6ea339e139b74eb61cf8777aeed06b6073
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:46:25 +0800

    Added an algorithm that can search for the places and added condition that it is a declared city during the 20th century.

--- BreadthFirstSearchFIFO.py
+++ BreadthFirstSearchFIFO.py
@@ -5,3 +5,12 @@ def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+#for loop that will scan if a city has been granted to a place from 20th century.
+for node in nx.bfs_tree(graph, nodes["oxford"]):
+    print(">>", node.name, node.year)
+    if is_twentieth_century(node.year):
+        print("\n\nFound:", node.name, node.year)
+        break
+else:
+    print("Not found")
