259d4d618cc7e7b53c52fd3404bd13609a33642e
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:18:36 +0800

    Remove comment tag on some of the codes to see if the program is working perfectly.

--- GraphTester.py
+++ GraphTester.py
@@ -2,18 +2,18 @@ from Graph import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
-# print("Prints the nodes of the selected " + str(nodes["oxford"]))
-# print(graph)
+print("Prints the nodes of the selected " + str(nodes["oxford"]))
+print(graph)
 
 
 #list of places/cities near oxford
-# print("Places that can be found near Oxford are: \n")
-# for neighbor in graph.neighbors(nodes["oxford"]):
-#     print(neighbor.name)
+print("Places that can be found near Oxford are: \n")
+for neighbor in graph.neighbors(nodes["oxford"]):
+    print(neighbor.name)
 
 #list of places/cities near oxford and its distance
-# for neighbor, interval in graph[nodes["oxford"]].items():
-#     print(interval["distance"], neighbor.name)
+for neighbor, interval in graph[nodes["oxford"]].items():
+    print(interval["distance"], neighbor.name)
 
 
 #sorted the nearest and farthest place near oxford using the sort_by and by_distance def functions.
