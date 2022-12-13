1a4a4c2eb23355383ce3262ab0f3956c3d7b9d15
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 17:04:03 +0800

    Fixed some typographical error

--- GraphTester.py
+++ GraphTester.py
@@ -5,8 +5,9 @@ nodes, graph = load_graph("roadmap.dot", City.from_dict)
 # print("Prints the nodes of the selected " + str(nodes["oxford"]))
 # print(graph)
 
+
 #list of places/cities near oxford
 print("Places that can be found near Oxford are: \n")
-for neighbor in graph.neighbors(nodes["gloucester"]):
+for neighbor in graph.neighbors(nodes["oxford"]):
     print(neighbor.name)
 
