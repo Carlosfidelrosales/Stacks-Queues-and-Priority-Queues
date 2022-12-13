f6324a0edb0cdfc30831a2193a68c331c3dc589c
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-13 18:03:50 +0800

    Added another program that will tell that a city is from 20th century and has the highest latitude of all the others.

--- BreadthFirstSearchFIFO.py
+++ BreadthFirstSearchFIFO.py
@@ -7,10 +7,26 @@ def is_twentieth_century(year):
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 #for loop that will scan if a city has been granted to a place from 20th century.
-for node in nx.bfs_tree(graph, nodes["oxford"]):
-    print(">>", node.name, node.year)
+# print("\nFinding the declared city stating from 20th Century.\n\n")
+# for node in nx.bfs_tree(graph, nodes["oxford"]):
+#     print(">>", node.name, node.year)
+#     if is_twentieth_century(node.year):
+#         print("\n\nFound:", node.name, node.year)
+#         break
+# else:
+#     print("Not found")
+
+#for loop that will scan if a city is from 20th century and it has the highest latitude among all the others.
+print("\nFinding the City that is from 20th Century and it has highest value of latitude.\n\n")
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+for node in nx.bfs_tree(graph, nodes["oxford"], sort_neighbors=order):
+    print(">>", node.name, node.year, node.latitude)
     if is_twentieth_century(node.year):
-        print("\n\nFound:", node.name, node.year)
+        print("\n\nFound:", node.name, node.year, node.latitude)
         break
 else:
     print("Not found")
