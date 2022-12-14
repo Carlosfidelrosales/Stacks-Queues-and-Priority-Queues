60a7d0f8147c5ac659d8c77ae4011df6169305b3
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-14 19:14:21 +0800

    Added another def function and a print to test the program.

--- DepthFirstLIFO.py
+++ DepthFirstLIFO.py
@@ -1,14 +1,32 @@
 import networkx as nx
-from Graph import City, load_graph
+from Graph import (
+    City,
+    load_graph,
+    depth_first_traverse,
+    depth_first_search as dfs
+)
 
-def is_twentieth_century(year):
-    return year and 1901 <= year <= 2000
+
+# def is_twentieth_century(year):
+#     return year and 1901 <= year <= 2000
+
+# nodes, graph = load_graph("roadmap.dot", City.from_dict)
+# for node in nx.dfs_tree(graph, nodes["oxford"]):
+#     print("->", node.name)
+#     if is_twentieth_century(node.year):
+#         print("\n\nFound:", node.name, node.year)
+#         break
+#     else:
+#         print("Not found")
+
+
+def is_twentieth_century(city):
+    return city.year and 1901 <= city.year <= 2000
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
-for node in nx.dfs_tree(graph, nodes["oxford"]):
-    print("->", node.name)
-    if is_twentieth_century(node.year):
-        print("\n\nFound:", node.name, node.year)
-        break
-    else:
-        print("Not found")
+city = dfs(graph, nodes["edinburgh"], is_twentieth_century)
+city.name
+
+
+for city in depth_first_traverse(graph, nodes["edinburgh"]):
+    print(city.name)
