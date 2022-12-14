6cefc858809b1c4e78f10b25fa3e6f8be7783ef6
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-14 18:05:46 +0800

    Added another exercise about shortest path.

--- ShortestPath.py
+++ ShortestPath.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from Graph import City, load_graph
+from Graph import shortest_path
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
@@ -8,3 +9,18 @@ city2 = nodes["preston"]
 
 for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
     print(f"{i}.", " → ".join(city.name for city in path))
+
+
+
+
+
+" → ".join(city.name for city in shortest_path(graph, city1, city2))
+
+
+def by_latitude(city):
+    return -city.latitude
+
+print(" → ".join(
+    city.name
+    for city in shortest_path(graph, city1, city2, by_latitude)
+))
