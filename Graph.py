17935bf2a594c6b20a97c5dddb0f62e91f131048
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-14 20:09:11 +0800

    Added a def function for dijkstra shortest path.

--- Graph.py
+++ Graph.py
@@ -3,6 +3,8 @@ import networkx as nx
 from QueuesFIFO import Queue
 from collections import deque
 from StackdatatypeLIFO import Queue, Stack
+from math import inf as infinity
+from StackdatatypeLIFO import MutableMinHeap, Queue, Stack
 
 class City(NamedTuple):
     name: str
@@ -83,13 +85,6 @@ def breadth_first_search(graph, source, predicate, order_by=None):
 def connected(graph, source, destination):
     return shortest_path(graph, source, destination) is not None
 
-
-
-
-
-
-
-
 def depth_first_traverse(graph, source, order_by=None):
     stack = Stack(source)
     visited = set()
@@ -124,4 +119,25 @@ def search(traverse, graph, source, predicate, order_by=None):
     for node in traverse(graph, source, order_by):
         if predicate(node):
             return node
+
+def dijkstra_shortest_path(graph, source, destination, weight_factory):
+    previous = {}
+    visited = set()
+
+    unvisited = MutableMinHeap()
+    for node in graph.nodes:
+        unvisited[node] = infinity
+    unvisited[source] = 0
+
+    while unvisited:
+        visited.add(node := unvisited.dequeue())
+        for neighbor, weights in graph[node].items():
+            if neighbor not in visited:
+                weight = weight_factory(weights)
+                new_distance = unvisited[node] + weight
+                if new_distance < unvisited[neighbor]:
+                    unvisited[neighbor] = new_distance
+                    previous[neighbor] = node
+
+    return retrace(previous, source, destination)
 
