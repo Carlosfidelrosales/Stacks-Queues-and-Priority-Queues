bcf0f208bc286200ead415736cedbedda81bf8a4
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-12-15 16:55:28 +0800

    Added the remaining def functions in the program and BeautifulSoup module has been utilized.

--- AsyncQueues.py
+++ AsyncQueues.py
@@ -64,3 +64,34 @@ async def worker(worker_id, session, queue, links, max_depth):
             print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
         finally:
             queue.task_done()
+
+
+async def fetch_html(session, url):
+    async with session.get(url) as response:
+        if response.ok and response.content_type == "text/html":
+            return await response.text()
+
+
+def parse_links(url, html):
+    soup = BeautifulSoup(html, features="html.parser")
+    for anchor in soup.select("a[href]"):
+        href = anchor.get("href").lower()
+        if not href.startswith("javascript:"):
+            yield urljoin(url, href)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type=int, default=2)
+    parser.add_argument("-w", "--num-workers", type=int, default=3)
+    return parser.parse_args()
+
+
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
